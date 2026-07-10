9517b36d874479788084e971d8be3524134be71e
techdevs1 <techdevsstudio@gmail.com>
2026-07-10 20:04:29 +0000

    reverting changes and working on the login redirection

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-290dd570'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.fukccemadtc"
+    "revision": "0.h02ev9r528g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
